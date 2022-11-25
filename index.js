079c6c68b1b9e72870bc994c31f8999bd55d5e4d
max <promitms7@gmail.com>
2022-11-25 13:44:22 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 4000;
-require('dotenv').config();
-
+require("dotenv").config();
 
 // middlewear
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send('Resale Bike Is Running')
-    
-})
+// MongoDb
 
-app.listen(port,()=>{
-    // client.connect(err => {
-    //    if(err){
-    //     console.log(err);
-    //    }
-    //    else{
-    //     console.log('Connected To Mongodb')
-    //    }
-        
-    //   });
-      
-    console.log('Server is Running on', port)
-})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ofikfyh.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+
+app.get("/", (req, res) => {
+  res.send("Resale Bike Is Running");
+});
+
+app.listen(port, () => {
+  client.connect(err => {
+     if(err){
+      console.log(err);
+     }
+     else{
+      console.log('Connected To Mongodb')
+     }
+
+    });
+
+  console.log("Server is Running on", port);
+});
